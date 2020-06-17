c0b0f35f6bdc99a1b9749df0a872f4c9ddc5f25c
Thiago <thiago.giachetto@gmail.com>
2020-06-17 09:13:06 -0300

    WIP - Improved class random variable

--- dypro/dypro/randomVariable.py
+++ dypro/dypro/randomVariable.py
@@ -12,17 +12,22 @@ class ValueProbability():
         return self.probability
     
     def __str__(self):
-        return f"{self.getProbability()} {self.getValue()}"
+        return f"{self.getValue()}:{self.getProbability()}"
 
-class RandomVariable(ValueProbability):
-    def __init__(self, *args):
-        if len(args)%2==0:
-            # add valueProb to list
-            pass
+class RandomVariable():
+    def __init__(self, values:list, probabilities:list):
+        if len(values) != len(probabilities):
+            raise Exception("Number of values is not equal probabilities")
+        elif sum(probabilities)!=1:
+            raise Exception("Sum of probabilities is not 1")
         else:
-            raise Exception("Number of arguments is not pair")
-        # super().__init__(value, probability)
+            self.__randomValue = [ValueProbability(a, b) for (a, b) in zip(values, probabilities)]
+    def randomValue(self):
+        return self.__randomValue
+    
+    def __str__(self):
+        return f"{'{'}{', '.join([str(v) for v in self.randomValue()])}{'}'}"
 
 if __name__ == "__main__":
-    print('bla')
-    RandomVariable(1,0.2, 2)
+    a = RandomVariable((1,2,3), (0.2,0.5,0.3))
+    print(a)
