b5dad98fe7d704f2bde35d87a7ac17dbd23f9fb4
Thiago <thiago.giachetto@gmail.com>
2020-08-22 23:26:58 -0300

    WIP added new method to solve stochastic problems

--- dypro/dypro/solve.py
+++ dypro/dypro/solve.py
@@ -1,5 +1,6 @@
 import numpy as np
 import logging
+import sys
 from dypro.dypro.utils import progressbar
 
 logging.basicConfig(level=logging.ERROR)
@@ -47,7 +48,7 @@ def solve(state, numberOfStages:int, finalStateCost, decision, solveInfeasibilit
         logging.debug(f"{FMap}")
     
     for k in range(numberOfStages-1, -1, -1): #n-1 to 0
-        for xk in progressbar(state(k)):
+        for xk in state(k):
             F_aux = inf
             u_aux = None
             
@@ -61,7 +62,8 @@ def solve(state, numberOfStages:int, finalStateCost, decision, solveInfeasibilit
                 else:
                     xk_next, _F = (xk_next_maybe_infeasible, FMap[k+1, xk_next_maybe_infeasible])
 
-                F_aux_uk = elementaryCost(k, xk, uk) + FMap[k+1, xk_next]
+                # F_aux_uk = elementaryCost(k, xk, uk) + FMap[k+1, xk_next]
+                F_aux_uk = elementaryCost(k, xk, uk) + _F
 
                 logging.debug(f"{k} {xk} {uk} {xk_next_maybe_infeasible} next: {xk_next} {F_aux_uk} {elementaryCost(k, xk, uk)}")
 
@@ -89,6 +91,82 @@ def solve(state, numberOfStages:int, finalStateCost, decision, solveInfeasibilit
     em k + 1 terei demanda=..., estoque=0, produzo=...
     """
 
+
+def solveStochastic(state, numberOfStages:int, finalStateCost, decision, solveInfeasibility, 
+            transitionFunction, elementaryCost, initialFMap=dict(), initialPolicy=dict(), inf=np.inf):
+    """ Solves the optimality recursive equation using a backward strategy
+
+    Parameters
+
+    state : function 
+            Returns all possible states for each stage
+    
+    numberOfStages : int
+            Number of stages
+    
+    finalStateCost : function 
+            Returns the cost of all final states
+    
+    decision : function
+            Returns all possible decisions for each stage
+    
+    solveInfeasibility : function of (k, xk_next, FMap)
+            Returns (feasible xk_next, _F)
+    
+    transitionFunction : function of (k, xk, uk)
+            Returns the next state
+    
+    elementaryCost : function of (k, xk, uk)
+            Returns the cost of decision on state
+    
+    initialMap : dict
+
+    initialPolicy : dict
+
+    Returns:
+        [(Map of costs : dict, policy : dict))]: [description]
+    """    
+
+    FMap = dict.copy(initialFMap)
+    policy = dict.copy(initialPolicy)
+
+    for xk in state(numberOfStages):
+        FMap[numberOfStages, xk] = finalStateCost(xk)
+        logging.debug(f"{FMap}")
+    
+    for k in range(numberOfStages-1, -1, -1): #n-1 to 0
+        for xk in state(k):
+            F_aux = inf
+            u_aux = None
+            
+            # Calculates the best decision on stage k and state uk
+            for uk in decision(k):
+                
+                # contains the expectation of the cost
+                E_F_aux = 0.0 
+
+                for wk in realizableRandomValues(k):
+                    xk_next_maybe_infeasible = transitionFunction(k, xk, uk, wk)
+                    
+
+                    if (k+1, xk_next_maybe_infeasible) not in FMap:
+                        xk_next, _F = solveInfeasibility(k+1, xk_next_maybe_infeasible, FMap)
+                    else:
+                        xk_next, _F = (xk_next_maybe_infeasible, FMap[k+1, xk_next_maybe_infeasible])
+
+                    E_F_aux += elementaryCost(k, xk, uk) + _F
+
+                    logging.debug(f"{k} {xk} {uk} {xk_next_maybe_infeasible} next: {xk_next} {F_aux_uk} {elementaryCost(k, xk, uk)}")
+
+
+                if F_aux > E_F_aux:
+                    F_aux = E_F_aux
+                    u_aux = uk
+            
+            FMap[k, xk] = F_aux
+            policy[k, xk] = u_aux
+    
+    return (FMap, policy)
 
 def optimalTrajectory(initialState, numberOfStages, policy, transitionFunction):
         """
